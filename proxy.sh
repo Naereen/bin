613cc0af149d7d04e375215162a2a291ea4be6df
Lilian Besson <lbesson@ens-cachan.fr>
2014-07-21 19:38:01 +0530

    Done : SOCKS proxying is intersting.

--- proxy.sh
+++ proxy.sh
@@ -2,14 +2,27 @@
 # (C) Lilian BESSON
 # http://besson.qc.to/bin/proxy.sh
 
-dom="${1:-zamok}"
+# The domain to connect with
+dom="${1:-zamok.crans.org}"
 shift
-url="${2:-monip.org}"
 
-echo "Starting SOCKS v5 Proxy : port:23456 host:${dom}.crans.org ... (press Enter to continue)"
-read
-xfce4-terminal --title="SOCKS v5 Proxy : port:23456 host:${dom}.crans.org ..." --command="ssh -v -D 23456 -N besson@${dom}.crans.org" &
+# The username to use
+login="${1:-besson}"
+shift
 
-echo "Starting Firefox with tsocks... (press Enter to continue)"
+# The command to launch
+mycommand="${1:-firefox}"
+shift
+
+# The args to pass to the command
+args="${@:-monip.org}"
+
+echo -e "Starting SOCKS v5 Proxy : login:${login} port:23456 host:${dom} ... (press Enter to continue)"
 read
-tsocks firefox -new-instance "${url}"
+xfce4-terminal --title="SOCKS v5 Proxy : login:${login} port:23456 host:${dom} ..." --command="ssh -p 443 -v -D 23456 -N ${login}@${dom}" &
+
+echo -e "Starting ${mycommand} with tsocks... (press Enter to continue)"
+read
+
+# tsocks firefox -new-instance "${args}"
+tsocks "${mycommand}" "${args}"
