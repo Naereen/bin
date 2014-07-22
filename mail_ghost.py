8bf881c4fcdd25f66f3206df5e5636250a083a53
Lilian Besson <lbesson@ens-cachan.fr>
2014-07-22 09:27:38 +0530

    Voilà c'est propre là.

--- mail_ghost.py
+++ mail_ghost.py
@@ -18,34 +18,35 @@ signature="""
 """
 
 def send_me_an_email(message, subj="[LOG] no object", me=defaultaddress,
-				 you=defaultaddress, my_identity="jarvis"):
-	"""	Send a message [message] by email.
-	The content of the email is [message], the subject is [subj].
-	The sender is [me], not necessary a valid sender for the SMTP.
-	The mail will be sent to [you].
+                 you=defaultaddress, my_identity="jarvis"):
+    """ Send a message [message] by email.
+    The content of the email is [message], the subject is [subj].
+    The sender is [me], not necessary a valid sender for the SMTP.
+    The mail will be sent to [you].
 
-	Auto configured with SMTP server on smtp.crans.org in the cr@ns server (https://www.crans.org).
+    Auto configured with SMTP server (smtp.crans.org) with SSL support for the cr@ns server (http://www.crans.org)
 
-	(c) Lilian Besson, 2012-2014.
-	"""
-	msg = MIMEText("%s" % (message+(signature % me)).replace("\n",'\n'), _charset="utf-8")
-	# The order is "important", to act like a real mail client !
-	msg['User-Agent'] = "smtplib.text/plain with python 2.7.6 on jarvis.crans.org (with http://besson.qc.to/bin/mail_ghost.py)"
-	# Identity
-	msg['From'] = my_identity+" <"+me+">"
-	msg['Reply-To'] = me
-	msg['To'] = you
-	msg['Subject'] = subj
-	# Send the message via our own SMTP server, but don't include the envelope header.
-	# s = smtplib.SMTP('localhost')
-	# s = smtplib.SMTP('smtp.crans.org', port=465) # Try 587 for starttls ?
-	s = smtplib.SMTP_SSL('smtp.crans.org', port=465) # Try 587 for starttls ?
-	# See https://docs.python.org/2/library/smtplib.html#smtplib.SMTP.login
-	s.sendmail(me, [you], msg.as_string() )
-	s.quit()
-	print "An email has been sent to <%s>, from %s <%s>." % (you, my_identity, me)
-	print "Title of the email : <%s>" % subj
-	print "Content of the email : \n%s" % message
+    (c) Lilian Besson, 2012-2014.
+    """
+    msg = MIMEText("%s" % (message+(signature % me)).replace("\n",'\n'), _charset="utf-8")
+    # The order is "important", to act like a real mail client !
+    msg['User-Agent'] = "smtplib.text/plain with python 2.7.6 on jarvis.crans.org (with http://besson.qc.to/bin/mail_ghost.py)"
+    # Identity
+    msg['From'] = my_identity+" <"+me+">"
+    msg['Reply-To'] = me
+    msg['To'] = you
+    msg['Subject'] = subj
+    # Send the message via our own SMTP server, but don't include the envelope header.
+    # s = smtplib.SMTP('localhost')
+    # s = smtplib.SMTP('smtp.crans.org', port=465) # Try 587 for starttls ?
+    s = smtplib.SMTP_SSL('smtp.crans.org', port=465) # Try 587 for starttls ?
+    # See https://docs.python.org/2/library/smtplib.html#smtplib.SMTP.login
+    s.login(b64decode('YmVzc29u'), b64decode(open('/home/lilian/crans.b64').readline()[:-1]) )
+    s.sendmail(me, [you], msg.as_string() )
+    s.quit()
+    print "An email has been sent to <%s>, from %s <%s>." % (you, my_identity, me)
+    print "Title of the email : <%s>" % subj
+    print "Content of the email : \n%s" % message
 
 if __name__ == '__main__':
   if '-h' in sys.argv or '--help' in sys.argv:
