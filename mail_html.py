d7aa43fe86b15728502efac3e1657699232fdca3
Lilian Besson <lbesson@ens-cachan.fr>
2014-07-22 09:27:38 +0530

    Voilà c'est propre là.

--- mail_html.py
+++ mail_html.py
@@ -18,38 +18,39 @@ signature="""<br>-- <br>%s<br>
 <img style=\"display: none; vibility: hidden;\" src=\"https://ga-beacon.appspot.com/UA-38514290-15/mail_html.py/""" + datetoday + "?pixel\" />"
 
 def send_me_an_email(message, subj="[LOG] no object", me=defaultaddress,
-	             you=defaultaddress, my_identity="jarvis (HTML)"):
-	"""	Send a message [message] by email.
-	The content of the email is [message] (encoded as HTML), the subject is [subj].
-	The sender is [me], not necessary a valid sender for the SMTP.
-	The mail will be sent to [you].
-	
-	Auto configured with SMTP server on localhost in the cr@ns server (http://www.crans.org)
+                 you=defaultaddress, my_identity="jarvis (HTML)"):
+    """ Send a message [message] by email.
+    The content of the email is [message] (encoded as HTML), the subject is [subj].
+    The sender is [me], not necessary a valid sender for the SMTP.
+    The mail will be sent to [you].
 
-	(c) Lilian Besson, 2014.
-	"""
-	msg = MIMEText("%s" % (message+(signature % me)).replace("\n",'\n'), _charset=None, _subtype="html")
-	# The order is "important", to act like a real mail client !
-	# Change "Content-Type" to "text/html"
-	msg['Content-Transfer-Encoding'] = "utf-8"
-	msg['Content-Type'] = "text/html"
-	msg['User-Agent'] = "smtplib.text/html with python 2.7.3 on jarvis.crans.org (with http://besson.qc.to/bin/mail_html.py)"
-	# Identity
-	msg['From'] = my_identity+" <"+me+">"
-	msg['Reply-To'] = me
-	msg['To'] = you
-	msg['Subject'] = subj
-	
-	# Send the message via our own SMTP server, but don't include the envelope header.
+    Auto configured with SMTP server (smtp.crans.org) with SSL support for the cr@ns server (http://www.crans.org)
+
+    (c) Lilian Besson, 2014.
+    """
+    msg = MIMEText("%s" % (message+(signature % me)).replace("\n",'\n'), _charset=None, _subtype="html")
+    # The order is "important", to act like a real mail client !
+    # Change "Content-Type" to "text/html"
+    msg['Content-Transfer-Encoding'] = "utf-8"
+    msg['Content-Type'] = "text/html"
+    msg['User-Agent'] = "smtplib.text/html with python 2.7.3 on jarvis.crans.org (with http://besson.qc.to/bin/mail_html.py)"
+    # Identity
+    msg['From'] = my_identity+" <"+me+">"
+    msg['Reply-To'] = me
+    msg['To'] = you
+    msg['Subject'] = subj
+
+    # Send the message via our own SMTP server, but don't include the envelope header.
     # s = smtplib.SMTP('localhost')
     # s = smtplib.SMTP('smtp.crans.org', port=465) # Try 587 for starttls ?
     s = smtplib.SMTP_SSL('smtp.crans.org', port=465) # Try 587 for starttls ?
     # See https://docs.python.org/2/library/smtplib.html#smtplib.SMTP.login
-	s.sendmail(me, [you], msg.as_string() )
-	s.quit()
-	print "A HTML email has been sent to <%s>, from %s <%s>." % (you, my_identity, me)
-	print "Title of the email : <%s>" % subj
-	print "Content of the email : \n%s" % message
+    s.login(b64decode('YmVzc29u'), b64decode(open('/home/lilian/crans.b64').readline()[:-1]) )
+    s.sendmail(me, [you], msg.as_string() )
+    s.quit()
+    print "A HTML email has been sent to <%s>, from %s <%s>." % (you, my_identity, me)
+    print "Title of the email : <%s>" % subj
+    print "Content of the email : \n%s" % message
 
 if __name__ == '__main__':
   if '-h' in sys.argv or '--help' in sys.argv:
