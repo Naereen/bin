bfdabca7e40a2b9398ae84bfac53068384fefb0e
Lilian Besson <lbesson@ens-cachan.fr>
2014-08-19 00:09:17 +0530

    Petits changement pour GenerateStatsMarkdown.sh

--- GenerateStatsMarkdown.sh
+++ GenerateStatsMarkdown.sh
@@ -3,18 +3,15 @@
 # Email: Lilian.BESSON[AT]ens-cachan[DOT]fr
 # Web version: http://besson.qc.to/bin/GenerateStatsMarkdown.sh
 # Web version (2): https://bitbucket.org/lbesson/bin/src/master/GenerateStatsMarkdown.sh
-# Date: 02-04-2013
+# Date: 18-08-2013
 #
 # A small script to create a minimalistic Markdown status page for my machine,
-# available with zamok proxying at http://besson.qc.to/stats.html (private ONLY)
-# or locally at http://0.0.0.0/stats.html
+# available ocally at http://0.0.0.0/stats.html
 #
 # Hack: this markdown page is using http://lbo.k.vu/md/ (StrapDown.js) to be a good-looking HTML page !
 #
-# FIXME: zamok proxying not yet available
-#
 BIN=GenerateStatsMarkdown
-version=1.2
+version=1.3
 
 # StrapDownJS nice themes : cyborg united
 theme="${2:-united}"
@@ -24,15 +21,16 @@ dest="${HOME}/Public/stats.html"
 case "$1" in
 #	TODO: Add more theme to StrapDown.js !
 #	# amelia|cerulean|cosmo|custom|cyborg|darkly|flatly|journal|lumen|readable|shamrock|simplex|slate|spacelab|spruce|superhero|united|yeti)
-	cyborg|united)
+	cyborg|united|bootstrap)
 		echo -e "${red}Using $1 as a theme option...${white}"
 		theme="$1"
 		shift
 		;;
 	-h|--help)
 		echo -e "${green}${BIN}${white} --help | [options]\n Creates a minimalistic statistics HTML report, to ${dest}.\n It uses http://lbo.k.vu/md/ (StrapDown.js) to improve the awesomeness of this mini munin clone."
-		echo -e "\nOptions:\n 1:\t${yellow}--help${white}\tto print this help,\n 1:\t${yellow}cron${white}\tto change logging behaviour (only if launched by cron),\n 1,2:\t${yellow}theme${white}\tin the list 'amelia', 'cerulean', 'cosmo', 'custom', 'cyborg', 'darkly', 'flatly', 'journal', 'lumen', 'readable', 'shamrock', 'simplex', 'slate', 'spacelab', 'spruce', 'superhero', 'united', 'yeti' (${cyan}Default and best is 'united', 'cyborg' is cool too${white}.)\n\n"
-		echo -e "$BIN v$version : Copyrights: (c) Lilian Besson 2014.\nReleased under the term of the GPL v3 Licence (more details on http://besson.qc.to/LICENSE.html).\nIn particular, $BIN is provided WITHOUT ANY WARANTY."
+		echo -e "\nOptions:\n 1:\t${yellow}--help${white}\tto print this help,\n 1:\t${yellow}cron${white}\tto change logging behaviour (only if launched by cron),\n 1,2:\t${yellow}theme${white}\t ${neg}united${Neg}, ${neg}bootstrap${Neg} or ${neg}cyborg${Neg} (${cyan}Default and best is ${neg}united${Neg}${white}.)\n\n"
+		#  the list 'amelia', 'cerulean', 'cosmo', 'custom', 'cyborg', 'darkly', 'flatly', 'journal', 'lumen', 'readable', 'shamrock', 'simplex', 'slate', 'spacelab', 'spruce', 'superhero', 'united', 'yeti' (${cyan}Default and best is 'united', 'cyborg' is cool too.
+		echo -e "$BIN v$version : Copyleft: (c) Lilian Besson 2014.\nReleased under the term of the GPL v3 Licence (more details on http://besson.qc.to/LICENSE.html).\nIn particular, $BIN is provided WITHOUT ANY WARANTY."
 		exit 0
 		;;
 	*)
@@ -51,7 +49,7 @@ fi
 echo -e "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"/><title>Stats pour jarvis.crans.org</title></head><body><xmp theme=\"${theme}\">" > "$dest"
 echo -e "# Informations systèmes pour *jarvis* ([jarvis.crans.org](http://jarvis.crans.org))" >> "$dest"
 echo -e "> #### Signaler *tout problème* à [jarvisATcransDOTorg](mailto:jarvisATcransDOTorg) ou via [bitbucket](https://bitbucket.org/lbesson/bin/issues/new).\n#### Données mises à jour le **$(date "+%c")**." >> "$dest"
-echo -e "> #### Consulter [*les rapports munin*](http://jarvis/lns_munin/localdomain/localhost.localdomain/index.html) (plus complets) ?\n\n***\n" >> "$dest"
+echo -e "> #### Consulter [*les rapports munin*](http://0.0.0.0/lns_munin/localdomain/localhost.localdomain/index.html) (plus complets) ?\n\n***\n" >> "$dest"
 
 MY_IP=$(/sbin/ifconfig | awk '/inet adr:/ { print $2 } ' | sed -e s/addr://)
 
@@ -64,7 +62,7 @@ landscape-sysinfo | head --lines=-3 | grep -v "^$" >> "$dest"
 echo -e "</pre>\n\n***\n\n## Utilisateurs connectés (\`w -h\`) *Normalement*, juste *lilian* !\n> <pre>" >> "$dest"
 w -h >> "$dest"
 
-echo -e "</pre>\n\n## Adresses IP\n> <pre>" >> "$dest"
+echo -e "</pre>\n\n## Adresse(s) IP\n> <pre>" >> "$dest"
 echo ${MY_IP:-"Not connected"} >> "$dest"
 
 echo -e "</pre>\n\n## Statut NGinx (\`nginx_status.sh\`)\n> <pre>" >> "$dest"
@@ -86,10 +84,10 @@ echo -e "</pre>\n\n## Série en cours (\`head -n 1 \"${HOME}\"/current\`)\n> <pr
 head -n 1 "${HOME}"/current >> "$dest"
 
 # Footer
-echo -e "</pre>\n\n***\n\n##### Mis-à-jour régulièrement via *cron*, avec [GenerateStatsMarkdown.sh](http://besson.qc.to/bin/GenerateStatsMarkdown.sh), un script Bash écrit par et pour [Lilian Besson](http://besson.qc.to/)." >> "$dest"
+echo -e "</pre>\n\n***\n\n##### Mis-à-jour régulièrement via *cron*, avec [GenerateStatsMarkdown.sh](http://besson.qc.to/bin/GenerateStatsMarkdown.sh) v${version}, un script Bash écrit par et pour [Lilian Besson](http://besson.qc.to/)." >> "$dest"
 
 ## FIXME add http://www.dptinfo.ens-cachan.fr/~lbesson/ before _static/
-echo -e "\n</xmp><script type=\"text/javascript\" src=\"_static/md/strapdown.js?src=GSM.sh\"></script>\n<img alt=\"GA|Analytics\" style=\"visibility: hidden; display: none;\" src=\"https://ga-beacon.appspot.com/UA-38514290-1/stats.html/theme=${theme}/?pixel\"/>\n</body></html>" >> "$dest"
+echo -e "\n</xmp><script type=\"text/javascript\" src=\"_static/md/strapdown.min.js?src=GSM.sh?beacon\"></script>\n<img alt=\"GA|Analytics\" style=\"visibility: hidden; display: none;\" src=\"https://ga-beacon.appspot.com/UA-38514290-1/stats.html/theme_${theme}/?pixel\"/>\n</body></html>" >> "$dest"
 
 # Notify the user
 if [ "X$1" = "Xcron" ]; then
