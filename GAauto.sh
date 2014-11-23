1219ca934f6d5cba5223d7d519a2abcd8ecdbf4b
Lilian Besson <lbesson@ens-cachan.fr>
2014-11-23 15:52:09 +0530

    Option --keep|-k for strapdown2pdf.
    [sub*]section{ --> section*{ now (by default, remove numbering).
    Force some links to be put with \texttt{}.

--- GAauto.sh
+++ GAauto.sh
@@ -24,19 +24,19 @@ case "$HOSTNAME" in
 	*jarvis*)
 		h="jarvis"
 		c="${PWD#/home/lilian/}"
-		c="${c#Public}"
+		c="${c#Public/}"
 		;;
 	*zamok*)
 		h="perso.crans.org/besson/"
 		key="UA-38514290-1"
-		c="${PWD#/home/besson/}"
-		c="${c#www}"
+		c="${PWD#/home/b/besson/}"
+		c="${c#www/}"
 		;;
 	*ssh*)
 		h="www.dptinfo.ens-cachan.fr/~lbesson/"
 		key="UA-38514290-2"
 		c="${PWD#/import/lbesson/}"
-		c="${c#public_html}"
+		c="${c#public_html/}"
 		;;
 	esac;
 
@@ -46,12 +46,12 @@ echo -e "${cyan}Adding Google Analytics beacon every where, v0.1${reset}"
 
 echo -e "Working on the domain '${red}$h${white}' with default path '${red}$c${white}',"
 
-n=$(find . -type f -iname '*'.html | wc -l)
+n=$(find . -type f -iname '*'.htm'*' | wc -l)
 
 echo -e "There is '${red}$n${white}' readable file(s) in the current dir, is that ok ? (it might take some time...)"
 read -e -p "Continue ? [Ctrl+C to cancel now or after]"
 
-find . -type f -iname '*'.html -exec AddGA.sh "$@" "${key}" "${c}" {} \; \
+find . -type f -iname '*'.htm'*' -exec AddGA.sh "$@" "${key}" "${c}" {} \; \
  | tee ~/GAauto.log \
  && echo -e "DONE :)" \
  || echo -e "ERROR :("
