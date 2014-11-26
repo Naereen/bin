585f3848d3616d8ec6c81e4d69313ab68d85788e
Lilian Besson <lbesson@ens-cachan.fr>
2014-11-26 15:36:33 +0530

    Petites améliorations sur AddGA.sh et GAauto.sh (pour ~/cs101 et ~/ma10[12])

--- GAauto.sh
+++ GAauto.sh
@@ -2,13 +2,14 @@
 # Author: Lilian BESSON
 # Email: Lilian.BESSON[AT]ens-cachan[DOT]fr
 # Website: https://bitbucket.org/lbesson/bin/src/master/GAauto.sh
-# Date: 28-02-2013
+# Date: 26-11-2013
 #
 # A small script to add a one-pixel beacon image to every HTML files in a directory.
 # Requires: AddGA.sh (http://besson.qc.to/bin/AddGA.sh)
 #
 
 clear
+PWD="$(pwd)"
 
 # Put here the default values for these variables
 # h: domain name (mysite.com for instance)
@@ -25,12 +26,28 @@ case "$HOSTNAME" in
 		h="jarvis"
 		c="${PWD#/home/lilian/}"
 		c="${c#Public/}"
+		case "${c}" in
+			cs* )
+				key="UA-38514290-13"
+				;;
+			ma* )
+				key="UA-38514290-12"
+				;;
+		esac
 		;;
 	*zamok*)
 		h="perso.crans.org/besson/"
 		key="UA-38514290-1"
 		c="${PWD#/home/b/besson/}"
 		c="${c#www/}"
+		case "${c}" in
+			cs* )
+				key="UA-38514290-13"
+				;;
+			ma* )
+				key="UA-38514290-12"
+				;;
+		esac
 		;;
 	*ssh*)
 		h="www.dptinfo.ens-cachan.fr/~lbesson/"
@@ -44,7 +61,8 @@ case "$HOSTNAME" in
 echo -e "${yellow}.: Lilian Besson presents :."
 echo -e "${cyan}Adding Google Analytics beacon every where, v0.1${reset}"
 
-echo -e "Working on the domain '${red}$h${white}' with default path '${red}$c${white}',"
+echo -e "Working on the domain '${red}$h${white}' with default path '${red}$c${white}'."
+echo -e "Using the Google Analytics key '${green}$key${white}'."
 
 n=$(find . -type f -iname '*'.htm'*' | wc -l)
 
@@ -52,6 +70,6 @@ echo -e "There is '${red}$n${white}' readable file(s) in the current dir, is tha
 read -e -p "Continue ? [Ctrl+C to cancel now or after]"
 
 find . -type f -iname '*'.htm'*' -exec AddGA.sh "$@" "${key}" "${c}" {} \; \
- | tee ~/GAauto.log \
+ | tee /tmp/GAauto.log \
  && echo -e "DONE :)" \
  || echo -e "ERROR :("
