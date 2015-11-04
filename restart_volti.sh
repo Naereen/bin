e133c9c7890c075c124393baf5b394767753392c
Lilian Besson <lbesson@ens-cachan.fr>
2015-11-04 14:17:22 +0100

    Brutal but more efficient.

--- restart_volti.sh
+++ restart_volti.sh
@@ -10,5 +10,18 @@
 # Licence: GPL v3
 #
 
+# Information
+echo -e "$0 launched at ${date}"
+echo -e "pidof volti : $(pidof volti)"  # Fails
+echo -e "pgrep volti : $(pgrep volti)"  # Works
+/usr/bin/pstree -a -h -s -c -U -p | grep --color=auto volti
+# Killing
+i=$(pgrep volti)
+echo -e "pkill volti..."
 pkill volti
+echo -e "kill ${i}..."
+kill "$i"
+# Restarting
+echo -e "Restarting volti... ('volti &')"
 volti &
+echo -e "Done. pidof volti : $(pgrep volti)"
