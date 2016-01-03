e42a9905569e6a7a328078bead647c2de340af52
Lilian Besson <lbesson@ens-cachan.fr>
2016-01-03 16:06:15 +0100

    https://github.com/esc/git-stats/pull/5

--- git-complete-stats.sh
+++ git-complete-stats.sh
@@ -34,22 +34,23 @@ done
 #test if the directory is a git
 git branch &> /dev/null || exit 3
 
-echo "$(basename $0): do statistics on this git repository."
-echo "Number of commits per author:"
+echo -e "$(basename $0): do statistics on this git repository."
+echo -e "  From https://github.com/esc/git-stats/"
+echo -e "\nNumber of commits per author:"
 git --no-pager shortlog "${END_AND_BEGIN[@]}" -sn --all
-AUTHORS=$(git shortlog "${END_AND_BEGIN[@]}" -sn --all | cut -f2 | cut -f1 -d' ')
+AUTHORS="$(git shortlog "${END_AND_BEGIN[@]}" -sn --all | cut -f2 | cut -f1 -d' ')"
 
 longtiret='----------------------------------------------------------------------------------------------------------'
 for a in $AUTHORS; do
-    echo -e "----------------${longtiret:0:${#a}}"
+    echo -e "\n----------------${longtiret:0:${#a}}"
     echo -e "Statistics for: $a"
-    echo -n "  Number of files changed: "
+    echo -n "  - Number of files changed: "
     git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | grep -v -e "^$" | cut -f3 | sort -iu | wc -l || true
-    echo -n "  Number of lines added: "
+    echo -n "  - Number of lines added: "
     git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f1 | awk '{s+=$1} END {print s}' || true
-    echo -n "  Number of lines deleted: "
+    echo -n "  - Number of lines deleted: "
     git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f2 | awk '{s+=$1} END {print s}' || true
-    echo -n "  Number of merges: "
+    echo -n "  - Number of merges: "
     git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --merges --author="$a" | grep -c '^commit' || true
 
 done
