1f791afe77299dcbd4cda554e3b10e53d66dfe0c
Lilian Besson <lbesson@ens-cachan.fr>
2016-01-12 09:29:54 +0100

    mymake.sh: v0.2 add colors and remove useless line.

--- mymake.sh
+++ mymake.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 # Author: Lilian BESSON, (C) 2015-oo
 # Email: Lilian.BESSON[AT]ens-cachan[DOT]fr
-# Date: 02-01-2016.
+# Date: 12-01-2016.
 # Web: https://bitbucket.org/lbesson/bin/src/master/mymake.sh
 #
-# A better 'make' command.
+# A top-recursive 'make' command.
 #
 # Usage: mymake.sh ARG1 ARG2 ...
 # A better make command, which goes up in the folder, until it finds a valid Makefile file.
 #
 # Licence: MIT Licence (http://lbesson.mit-license.org).
-version="0.1"
+version="0.2"
 
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# More details at http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
-
+# Use https://bitbucket.org/lbesson/bin/src/master/.color.sh to add colors in Bash scripts
 . ~/.color.sh
+
 # Add here options
 NOANSI='false'
 JUSTVERSION='false'
+MAKEPATH='/usr/bin/make'
+
 for i in "$@"; do
     case "$i" in
         --noansi )
@@ -34,10 +37,10 @@ for i in "$@"; do
 done
 
 # Copyrights
-echo -e "${black}$0 v${version} : copyright (C) 2016 Lilian BESSON" >/dev/stderr
-echo -e "You can find it online (https://bitbucket.org/lbesson/bin/src/master/mymake.sh)" >/dev/stderr
-echo -e "This is free software, and you are welcome to redistribute it under certain conditions." >/dev/stderr
-echo -e "This program comes with ABSOLUTELY NO WARRANTY; for details see http://lbesson.mit-license.org${white}" >/dev/stderr
+echo -e "${cyan}$(basename "$0") v${version} : copyright (C) 2016 Lilian Besson${white}" >/dev/stderr
+echo -e "${black}You can find it online (https://bitbucket.org/lbesson/bin/src/master/mymake.sh)${white}" >/dev/stderr
+echo -e "${black}This is free software, and you are welcome to redistribute it under certain conditions.${white}" >/dev/stderr
+echo -e "${black}This program comes with ABSOLUTELY NO WARRANTY; for details see http://lbesson.mit-license.org${white}" >/dev/stderr
 [ "X${JUSTVERSION}" = "Xtrue" ] && exit 1
 
 
@@ -52,12 +55,14 @@ while [ ! -f "${old}${c}Makefile" ]; do
     cd "${c}"
     [ "$(pwd)" = "/" ] && break
 done
+
+# New using it to execute make
 if [ -f "${old}${c}Makefile" ]; then
     echo -e "${green}${old}${c}Makefile${white} is there, I'm using it :"
-    echo -e time /usr/bin/make -w --file="${old}${c}Makefile" $@
-    echo -e "notify-send --icon=make 'mymake.sh' \"make '$*', done in the folder '${old}${c}.\""
+    echo -e "Calling... ${black}'" time /usr/bin/make -w --file="${old}${c}Makefile" $@ "'${white}..."
+    echo -e "Then if success calling... ${black}'" notify-send --icon=make 'mymake.sh' "\"make '$*', done in the folder '${old}${c}.\"" "'${white}..."
     # read  # DEBUG
-    time /usr/bin/make -w --file="${old}${c}Makefile" $@ && \
+    time "${MAKEPATH}" -w --file="${old}${c}Makefile" $@ && \
         notify-send --icon=make 'mymake.sh' "make '$*', done in the folder '${old}${c}."
     cd "${old}"
 else
