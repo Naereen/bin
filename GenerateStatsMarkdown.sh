462b763c55aa19c564ebfc7794f8d9ca65790c38
Lilian Besson <lbesson@ens-cachan.fr>
2016-04-29 22:01:45 +0200

    Small improvements on GenerateStatsMarkdown.sh, and update list of StrapDown.js themes (https://github.com/Naereen/StrapDown.js/tree/master/themes).

--- GenerateStatsMarkdown.sh
+++ GenerateStatsMarkdown.sh
@@ -1,42 +1,44 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 # Author: Lilian BESSON
 # Email: Lilian.BESSON[AT]ens-cachan[DOT]fr
 # Web version: http://perso.crans.org/besson/bin/GenerateStatsMarkdown.sh
 # Web version (2): https://bitbucket.org/lbesson/bin/src/master/GenerateStatsMarkdown.sh
-# Date: 20-02-2016
+# Date: 29-04-2016
 #
 # A small script to create a minimalistic Markdown status page for my machine, available locally at http://0.0.0.0/stats.html
 #
 # Hack: this markdown page is using http://lbo.k.vu/md/ (StrapDown.js) to be a good-looking HTML page !
 #
 BIN=GenerateStatsMarkdown
-version=1.6
+version=1.7
 
-# StrapDownJS nice themes : cyborg united
+# StrapDownJS nicest themes : united
 theme="${2:-united}"
 dest="${HOME}/Public/stats.html"
 
 # Argument handling
 case "$1" in
-	cyborg|united|bootstrap|darkly|lumen|paper|simplex)
+	bootstrap|cerulean|cosmo|cyborg|darkly|flatly|journal|lumen|paper|readable|sandstone|simplex|slate|spacelab|superhero|united|yeti)
+		# cyborg|united|bootstrap|darkly|lumen|paper|simplex)
 		echo -e "${red}Using $1 as a theme option...${white}"
 		theme="$1"
 		shift
 		;;
 	-h|--help)
-		echo -e "${green}${BIN}${white} --help | [options]\n Creates a minimalistic statistics HTML report, to ${dest}.\n It uses http://lbo.k.vu/md/ (StrapDown.js) to improve the awesomeness of this mini munin clone."
-		echo -e "\nOptions:\n 1:\t${yellow}--help${white}\tto print this help,\n 1:\t${yellow}cron${white}\tto change logging behaviour (only if launched by cron),\n 1,2:\t${yellow}theme${white}\t ${neg}united${Neg}, ${neg}bootstrap${Neg}, ${neg}darkly${Neg}, ${neg}paper${Neg}, ${neg}lumen${Neg}, ${neg}simplex${Neg} or ${neg}cyborg${Neg} (${cyan}Default and best is ${neg}united${Neg}${white}.)\n\n"
-		echo -e "$BIN v$version : Copyleft: (c) Lilian Besson 2014-16.\nReleased under the term of the GPL v3 Licence (more details on http://perso.crans.org/besson/LICENSE.html).\nIn particular, $BIN is provided WITHOUT ANY WARANTY."
+		echo -e "${green}${BIN}${white} --help | [options]\n Creates a minimalistic statistics HTML report, to ${u}${black}${dest}${U}${white}.\n It uses ${u}${black}http://lbo.k.vu/md/${U}${white} (${cyan}StrapDown.js${white}) to improve the awesomeness of this mini munin clone."
+		echo -e "\n${red}Options:${white}\n 1:\t${yellow}--help${white}\tto print this help,\n 1:\t${yellow}cron${white}\tto change logging behaviour (only if launched by cron),\n 1,2:\t${yellow}theme${white}\t${neg}bootstrap${Neg}, ${neg}cerulean${Neg}, ${neg}cosmo${Neg}, ${neg}cyborg${Neg}, ${neg}darkly${Neg}, ${neg}flatly${Neg}, ${neg}journal${Neg}, ${neg}lumen${Neg}, ${neg}paper${Neg}, ${neg}readable${Neg}, ${neg}sandstone${Neg}, ${neg}simplex${Neg}, ${neg}slate${Neg}, ${neg}spacelab${Neg}, ${neg}superhero${Neg}, ${neg}united${Neg}, ${neg}yeti${Neg} (${cyan}Default and best is ${neg}united${Neg}${white}.)\n\n"
+		echo -e "${green}$BIN v$version : Copyleft: (c) Lilian Besson 2014-16.\nReleased under the term of the GPL v3 Licence (more details on http://perso.crans.org/besson/LICENSE.html).${white}\nIn particular, $BIN is provided WITHOUT ANY WARANTY."
 		exit 0
 		;;
 	*)
 		;;
 esac
+
 echo -e "${yellow}.: Lilian Besson presents :."
 echo -e "${cyan}${BIN}, v${version}${reset}"
 
 theme="${theme:-united}"
-echo -e "${cyan}The report will be written to : ${dest}${white}, with the theme ${magenta}${theme}${white}."
+echo -e "${white}The report will be written to : ${black}${u}${dest}${U}${white}, with the theme ${magenta}${theme}${white}."
 if [ -f "$dest" ]; then
 	cp -vf "$dest" /tmp/
 fi
@@ -62,7 +64,8 @@ echo -e "</pre>\n\n## Adresse(s) IP\n> <pre>" >> "$dest"
 echo "${MY_IP:-"Not connected"}" >> "$dest"
 
 echo -e "</pre>\n\n## [Statut NGinx](lns_munin/localdomain/localhost.localdomain/index.html#nginx) (\`nginx_status.sh\`)\n> <pre>" >> "$dest"
-/home/lilian/bin/nginx_status.sh >> "$dest"
+# /home/lilian/bin/nginx_status.sh >> "$dest"
+nginx_status.sh >> "$dest"
 
 echo -e "</pre>\n\n## [Durée d'activité](lns_munin/localdomain/localhost.localdomain/uptime.html) (\`uptime\`)\n> <pre>" >> "$dest"
 uptime >> "$dest"
@@ -86,15 +89,17 @@ echo -e "</pre>\n\n## Stats <a href='https://wakatime.com/dashboard'>WakaTime</a
 echo -e "\n<figure><embed width='680' type='image/svg+xml' src='https://wakatime.com/@lbesson/5d1ec603-73b0-44b9-b61e-5eeda2490e51.svg'></embed></figure>" >> "$dest"
 echo -e "\n<figure><embed width='680' type='image/svg+xml' src='https://wakatime.com/@lbesson/9f6c0b0b-6806-4afa-9a4e-651ee6201be0.svg'></embed></figure>" >> "$dest"
 
+type selfstats >/dev/null
+if [ "X$?" = "X0" ]; then
+	echo -e "\n\n## <a href='http://jarvis/selfvis.html'>Ratio clicks/keystrokes</a> (\`selfstats --human-readable --ratios\`)\n> <pre>" >> "$dest"
+	selfstats --human-readable --ratios | sed '/^$/d' >> "$dest"
 
-echo -e "\n\n## <a href='http://jarvis/selfvis.html'>Ratio clicks/keystrokes</a> (\`selfstats --human-readable --ratios\`)\n> <pre>" >> "$dest"
-selfstats --human-readable --ratios | sed '/^$/d' >> "$dest"
+	echo -e "</pre>\n\n## <a href='http://jarvis/selfvis.html'>Summary of clicks</a> (\`selfstats --human-readable --clicks\`)\n> <pre>" >> "$dest"
+	selfstats --human-readable --clicks | sed '/^$/d' >> "$dest"
 
-echo -e "</pre>\n\n## <a href='http://jarvis/selfvis.html'>Summary of clicks</a> (\`selfstats --human-readable --clicks\`)\n> <pre>" >> "$dest"
-selfstats --human-readable --clicks | sed '/^$/d' >> "$dest"
-
-echo -e "</pre>\n\n## <a href='http://jarvis/selfvis.html'>Stats</a> <a href='https://github.com/gurgeh/selfspy#example-statistics'>selfspy</a> (\`selfstats --min-keys 10 --back 1w --human-readable --pactive\`)\n> <pre>" >> "$dest"
-selfstats --min-keys 10 --back 1w --human-readable --pactive | sed '/^$/d' >> "$dest"
+	echo -e "</pre>\n\n## <a href='http://jarvis/selfvis.html'>Stats</a> <a href='https://github.com/gurgeh/selfspy#example-statistics'>selfspy</a> (\`selfstats --min-keys 10 --back 1w --human-readable --pactive\`)\n> <pre>" >> "$dest"
+	selfstats --min-keys 10 --back 1w --human-readable --pactive | sed '/^$/d' >> "$dest"
+fi
 
 # Footer
 echo -e "</pre>\n\n***\n\n##### Mis-à-jour régulièrement via *cron*, avec [GenerateStatsMarkdown.sh](http://perso.crans.org/besson/bin/GenerateStatsMarkdown.sh) v${version}, un script Bash écrit par et pour [Lilian Besson](http://perso.crans.org/besson/)." >> "$dest"
