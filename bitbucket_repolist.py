378974c89dcab7b1bf120ee847fe84367d204eb0
Lilian Besson <lbesson@ens-cachan.fr>
2016-06-02 09:32:05 +0200

    Update, clean up, add documentation, and full Python 3K compatibility.

--- bitbucket_repolist.py
+++ bitbucket_repolist.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
-# (c) 29-11-2013
-# by Lilian Besson (mailto:lbessonATens-cachanDOTfr)
-import json, os
+# -*- coding: utf-8; mode: python -*-
+"""
+A minimalist script to print the (ordered) list of Bitbucket repositories for a user :
 
-pseudo = "lbesson"
-os.system("curl https://bitbucket.org/api/1.0/users/" + pseudo + " > bitbucket.json")
+$ bitbucket_repolist.py pseudo
+git_repo1
+...
+git_repoK
 
-b = json.load(open('bitbucket.json'))
-for i in b['repositories']:
- print i['slug']
+- Date: 29-11-2013.
+- Author: Lilian Besson, (C) 2016.
+- Online: https://bitbucket.org/lbesson/bin/
+- Licence: MIT Licence (http://lbesson.mit-license.org).
+"""
+
+from __future__ import print_function  # Python 2/3 compatibility !
+from json import load
+from os import system
+from sys import argv
+
+# Read the pseudo from the command line, or use mine.
+pseudo = argv[1] if len(argv) > 1 else "lbesson"
+jsonfile = "bitbucket_{}.json".format(pseudo)
+system("curl --silent https://bitbucket.org/api/1.0/users/" + pseudo + " > " + jsonfile)
+
+b = load(open(jsonfile, 'r'))
+
+# for i in b['repositories']:
+#     print(i['slug'])
+list_of_repo = sorted([i['slug'] for i in b['repositories']])
+for i in list_of_repo:
+    print(i)
