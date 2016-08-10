5b9d6ca7ac120bc9d2b447f7d3cddafa169290ac
Lilian Besson <lbesson@ens-cachan.fr>
2016-08-10 21:10:11 +0200

    Better: exits earlier if not in a git, and prints correctly for each file.

--- git-count-modified-lines.sh
+++ git-count-modified-lines.sh
@@ -5,19 +5,40 @@
 # (C) Lilian BESSON
 # http://perso.crans.org/besson/bin/git-count-modified-lines.sh
 
-echo -e "${yellow}${u}Modified files${U} in this git repository ...${white}"
-git status --porcelain | grep '^ \?M' | sed s/'^ \?M '/'  '/
+# Are we in a git repo?
+git rev-parse --is-inside-work-tree &>/dev/null
+if [[ $? != 0 ]]; then
+    echo -e "${red}Not a git repository.${white}"
+    exit 1
+fi
 
-files=$(git status --porcelain | grep '^ \?M' | sed s/'^ \?M '/''/)
+prefix="$(git rev-parse --show-prefix)"
+p="$(pwd)/"
+maingit="${p%$prefix}"
+
+cd "${maingit}" &>$null
+
+# Start
+echo -e "${yellow}${u}Modified files${U} in this git repository ...${white}"
+files=$(git status --porcelain | grep '^ \?MM\?' | sed s/'^ \?MM\? '/''/)
 
 for f in $files; do
-    echo -e "\n${blue}For the file${white} '${black}${u}${f}${U}${white}' :"
-
-    nbplus=$(git diff --no-color | cat | grep -o '^+' | wc -l)
-    echo -e "  + ${green}${nbplus}${white} ${magenta}new lines${white} ..."
-
-    nbminus=$(git diff --no-color | cat | grep -o '^-' | wc -l)
-    echo -e "  - ${red}${nbminus}${white} ${cyan}deleted lines${white} ..."
+    if [ -f "$f" ]; then
+        echo -e "  ${f}"
+    fi
 done
 
+for f in $files; do
+    if [ -f "$f" ]; then
+        echo -e "\n${blue}For the file${white} '${black}${u}${f}${U}${white}' :"
+
+        nbplus=$(git diff --no-color -- "$f" | cat | grep -o '^+' | wc -l)
+        echo -e "  + ${green}${nbplus}${white} ${magenta}new lines${white} ..."
+
+        nbminus=$(git diff --no-color -- "$f" | cat | grep -o '^-' | wc -l)
+        echo -e "  - ${red}${nbminus}${white} ${cyan}deleted lines${white} ..."
+    fi
+done
+
+cd "${p}" &>$null
 echo -e "${reset}${white}"
