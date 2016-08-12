1fd6cb49ab0c72aa947a730c3084f8341cba1de4
Lilian Besson <lbesson@ens-cachan.fr>
2016-08-12 16:43:39 +0200

    Finally, this 'mymake.sh' script does EXACTLY what I want. On all my machines I hope...

--- mymake.sh
+++ mymake.sh
@@ -4,17 +4,16 @@
 # Date: 12-08-2016.
 # Web: https://bitbucket.org/lbesson/bin/src/master/mymake.sh
 #
-# A top-recursive 'make' command.
+# A top-recursive 'make' command, with two awesome behaviors.
 #
 # Usage: mymake.sh ARG1 ARG2 ...
-# A better make command, which goes up in the folder, until it finds a valid Makefile file.
+#   - A better make command, which goes up in the folder, until it finds a valid Makefile file.
+#   - Additionally, it keeps looking as long as no rule has been found.
 #
 # Licence: MIT Licence (http://lbesson.mit-license.org).
 #
-# DONE: Improved mymake.sh to keep looking recursively in the parent folder if a rule is not found in the current Makefile
-# https://bitbucket.org/lbesson/bin/issues/4/improve-mymakesh-to-keep-looking
 #
-version="0.6"
+version="0.7"
 returncode="0"  # If success, return 0
 
 # More details at http://redsymbol.net/articles/unofficial-bash-strict-mode/
@@ -25,16 +24,6 @@ set -o pipefail
 # options
 JustVersion="false"
 JustBashCompletion="false"
-
-LogFile="$(tempfile)"
-mv "${LogFile}" "${LogFile}_mymake.log"
-LogFile="${LogFile}_mymake.log"
-# echo -e "DEBUG LogFile = ${LogFile}"
-
-# Try to detect automatically the location of the make binary
-makePath="$(type make | awk ' { print $3 } ')"
-# But if it failed, use the default one
-makePath="${makePath:-/usr/bin/make}"
 
 for i in "$@"; do
     case "$i" in
@@ -62,6 +51,19 @@ if [ "X${JustVersion}" = "Xtrue" ]; then
     exit 1
 fi
 
+# Working variables
+LogFile="$(tempfile)"
+mv "${LogFile}" "${LogFile}_mymake.log"
+LogFile="${LogFile}_mymake.log"
+# echo -e "DEBUG LogFile = ${LogFile}"
+
+# Try to detect automatically the location of the make binary
+makePath="$(type make | awk ' { print $3 } ')"
+# But if it failed, use the default one
+makePath="${makePath:-/usr/bin/make}"
+
+OriginalPath="$(pwd)/"
+
 # The big loop: as long as we fail because there is no valid rule, go up
 FailBecauseNoValidRule="true"
 
@@ -75,7 +77,7 @@ while [ "X$FailBecauseNoValidRule" = "Xtrue" ]; do
     while [ ! -f "${old}${c}Makefile" ]; do
         echo -e "${red}${old}${c}Makefile${white} is not there, going up in the parent folder ... Current directory: $(pwd)"
         c="../${c}"
-        cd "${c}"
+        cd ..  # FIXED We should just go up once.
         [ "$(pwd)" = "/" -o "$(pwd)" = "${HOME}" ] && break  # DEBUG avoid infinite loops!
     done
     # We found a valid Makefile or we already called 'break'...
@@ -96,32 +98,32 @@ while [ "X$FailBecauseNoValidRule" = "Xtrue" ]; do
                 2>&1 | tee "${LogFile}"
         fi
         returncode="$?"
-        # echo -e "DEBUG returncode = ${returncode}"
-        # No notifications if make has been called by the bash auto-completion function (options '-npq -C'...)
-        if [ "X${JustBashCompletion}" != "Xtrue" ]; then
-            if [ "X${returncode}" = "X0" ]; then
-                   notify-send --icon=terminal 'mymake.sh' "make '$*', succeed in the folder '$(readlink -f "${old}${c}")' :-)"
-            else
-               notify-send --icon=error 'mymake.sh' "make '$*', failed in the folder '$(readlink -f "${old}${c}")' ..."
-            fi
-        fi
+
         # XXX This is very specific, maybe there is a better way to detect it ?
         # Not with the returned error code of /usr/bin/make at least...
         grep 'make: \*\*\* No rule to make target' "${LogFile}" &>/dev/null
-        if [[ "X$?" = "X0" ]]; then
+        grepreturncode="$?"
+        if [[ "X${grepreturncode}" = "X0" ]]; then
             echo -e "${red}Failed because there is no rule${white} to make the target '${yellow}$*${white}' in the current Makefile (${green}${NameOfMakefile}${white}) ..."
             [ "$(pwd)" = "/" -o "$(pwd)" = "${HOME}" ] && break  # avoid infinite loops!
             FailBecauseNoValidRule="true"
-            cd "${c}"
             c="../${c}"
+            cd ..  # FIXED We should just go up once.
             echo -e "${magenta}Going up in the parent folder...${white} Current directory: $(pwd)"
+            # [ "$(dirname $(pwd))" = "/" ] && break  # DEBUG avoid infinite loops!
         else
-            cd "${old}"
+            # No notifications if make has been called by the bash auto-completion function (options '-npq -C'...) or if it failed because no rule
+            if [ "X${JustBashCompletion}" != "Xtrue" ]; then
+                if [ "X${returncode}" = "X0" ]; then
+                       notify-send --icon=terminal "$(basename "$0") v${version}" "make '$*' worked, in the folder '$(readlink -f "${old}${c}")' from '$(readlink -f "${OriginalPath}")' :-)"
+                else
+                   notify-send --icon=error "$(basename "$0") v${version}" "make '$*' failed ,in the folder '$(readlink -f "${old}${c}")' from '$(readlink -f "${OriginalPath}")' ..."
+                fi
+            fi
         fi
     else
         # If we did not find any valid Makefile
         echo -e "${red}${NameOfMakefile} is not there and I'm in '/' or '${HOME}'... I cannot go up anymore${white}"
-        cd "${old}"
         exit 3
     fi
 done
