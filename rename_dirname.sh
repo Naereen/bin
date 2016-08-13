ee3f88ca32fba8c1fde316e837fc40945be29238
Lilian Besson <lbesson@ens-cachan.fr>
2016-08-13 15:46:24 +0200

    I finally found the time to fix this bug: my photos were renamed with 1..9 then 10..99 then 100..999, now it is 001..009 then 010..099 : no problem of wrong lexicographic ordering!

--- rename_dirname.sh
+++ rename_dirname.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # author: Lilian BESSON
 # email: Lilian.BESSON[AT]ens-cachan[DOT]fr
-# date: 01-05-2015.
+# date: 13-08-2016.
 # web: https://bitbucket.org/lbesson/bin/src/master/rename_dirname.sh
 #
 # Rename every picture in a directory based on the name of directory (not recursive).
 #
 # Note: Will ask confirmation before doing it.
 #
-# Example: rename_dirname.sh pdf
-# Example: rename_dirname.sh JPG
-#  will rename each PDF or JPEG file in the current dir.
+# Example: rename_dirname.sh pdf [pdf [Common_part_for_new_names]]
+# Example: rename_dirname.sh JPG jpg
+#    will rename each pdf or JPEG file in the current dir.
 #
 # Licence: GPLv3 (http://perso.crans.org/besson/LICENSE.html)
 #
@@ -23,37 +23,39 @@ destext="${2:-$smallext}"
 echo -e "${yellow}rename_dirname.sh${white} working with the extension ${red}${ext}${white} and the destination extension ${red}${destext}${white}."
 
 namedir="$( smoothnameone.sh "$(basename "$(pwd)")" )"
+namedest="${3:-$namedir}"
 echo -e "${magenta}Working in the directory ${white}${u}${namedir}${reset}.${white}"
 
 nb=$( find . -maxdepth 1 -iname '*'."${ext}" | wc -l )
 echo -e "${blue}I found ${u}${nb}${U} file(s) of this extension ${ext}.${white}"
 
+nbOfZeros=$( python -c "import math; print(int(math.ceil(math.log10($nb))))" )
 declare -i n
 
 # First we print what will be executed
 echo -e "${green}I am showing you the operations I want to do:${white}"
 
 n=0
-for myfile in $( find . -maxdepth 1 -iname '*'."${ext}" ); do
+for myfile in $( find . -maxdepth 1 -iname '*'."${ext}" | sort ); do
     n=n+1
-    dest="${namedir}__${n}.${destext}"
+    dest="${namedest}__$(printf "%.${nbOfZeros}i" ${n}).${destext}"
     if [ -f "${dest}" ]; then
-        echo -e "${red}The destination file ${u}${dest}${U} already exists, we move it to /tmp/.${white}"
-        echo mv -vf "${dest}" /tmp/
+        echo -e "       ${red}The destination file ${u}${dest}${U} already exists, we move it to /tmp/.${white}"
+        echo -e "   " mv -vf "'${dest}'" /tmp/
     fi
-    echo mv -vi "${myfile}" "${dest}"
+    echo -e "    " mv -vi "'${myfile}'" "'${dest}'"
 done
 
-echo -e "${green}Are you happy about these operations?${white}"
-echo "Enter to continue, Ctrl+C to exit now "
+echo -e "\n${green}Are you happy about these operations?${white}"
+echo "Enter to continue, Ctrl+C to exit now"
 read || exit
 
-echo -e "${blue}OK? Lets go :)${white}"
+echo -e "${blue}OK ? Lets go :)${white}"
 
 n=0
-for myfile in $( find . -maxdepth 1 -iname '*'."${ext}" ); do
+for myfile in $( find . -maxdepth 1 -iname '*'."${ext}" | sort ); do
     n=n+1
-    dest="${namedir}__${n}.${destext}"
+    dest="${namedest}__$(printf "%.${nbOfZeros}i" ${n}).${destext}"
     if [ -f "${dest}" ]; then
         echo -e "${red}The destination file ${u}${dest}${U} already exists, we move it to /tmp/.${white}"
         mv -vf "${dest}" /tmp/
@@ -62,3 +64,4 @@ for myfile in $( find . -maxdepth 1 -iname '*'."${ext}" ); do
 done
 
 echo -e "${green}Done :)${white}"
+# End of rename_dirname.sh
