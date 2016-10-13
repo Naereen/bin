f68439bb2449d6de3d19be937da1f5744b1d52e4
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-13 08:31:25 +0200

    Better: keep the trailing '.git/', and uses one call to 'find' instead of 'ls' (should be faster).

--- list_git_repos_in_current_folder.sh
+++ list_git_repos_in_current_folder.sh
@@ -5,4 +5,6 @@
 #
 # (C) Lilian Besson, 2016
 
-( for i in $(/bin/ls ./*/.git/config | sort | uniq); do echo "$i" | grep -o "\./[^/]*" ; done ) | grep -v "\./\.git" | sed s_'./'_''_
+# ( for i in $(/bin/ls ./*/.git/config | sort | uniq); do echo "$i" | grep -o "\./[^/]*" ; done ) | grep -v "\./\.git" | sed s_'./'_''_
+# Better: keep the trailing '.git/'
+for d in $(find -maxdepth 1 -type d); do [ -f $d/.git/config ] && echo $d; done | sort | sed s_'./'_''_
