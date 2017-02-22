785786a70d7b93131a147928c870be0503b2b38e
Lilian Besson <lbesson@ens-cachan.fr>
2017-02-22 22:43:13 +0100

    First try of a scrapper written in Python using the wonderful http://ScraPy.org package

---
+++ youtube_playlist_spider_scrapy.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8; mode: python -*-
+""" A small scrapper (using Scrapy) to extract URL of some songs in my YouTube "Watch Later" playlist.
+
+- *Date:* 22/02/2017.
+- *Author:* Lilian Besson © 2017.
+- *Licence:* MIT Licence (http://lbesson.mit-license.org).
+"""
+
+from __future__ import print_function  # Python 2 compatibility if needed
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "Watch Later"
+    start_urls = [
+        'file:///tmp/wl.html'
+    ]
+
+    def parse(self, response):
+        i = 0
+        for item in response.css('td.pl-video-title'):
+            i += 1
+            video = item.css('a.pl-video-title-link')[0]
+            author = item.css('div.pl-video-owner')[0]
+            yield {
+                'id': i,
+                'href': video.xpath('@href').extract_first().replace('&index=%i&list=WL'%i, ''),
+                'title': video.css('a::text').extract_first().strip(),
+                'author': author.css('a::text').extract_first().strip()
+            }
+
+
+# End of youtube_playlist_spider_scrapy.py
