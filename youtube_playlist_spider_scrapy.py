6cb2fd3c79a6b2a0042a36c7133f1fc9ef54ad70
Lilian Besson <lbesson@ens-cachan.fr>
2017-04-01 23:00:34 +0200

    Better, removed DEBUG output, and just print the result, no log

--- youtube_playlist_spider_scrapy.py
+++ youtube_playlist_spider_scrapy.py
@@ -1,37 +1,50 @@
-#! /usr/bin/env python3
+#! /usr/local/bin/scrapy runspider
 # -*- coding: utf-8; mode: python -*-
 """ A small scrapper (using Scrapy) to extract URL of some songs in my YouTube "Watch Later" playlist.
 
-- *Date:* 22/02/2017.
+- *Reference:* https://doc.scrapy.org/
+- *Date:* 01/04/2017.
 - *Author:* Lilian Besson © 2017.
 - *Licence:* MIT Licence (http://lbesson.mit-license.org).
 """
 
 from __future__ import print_function  # Python 2 compatibility if needed
+
+import logging
 import scrapy
+
+# Trying to disable logging
+logging.getLogger('scrapy').setLevel(logging.WARNING)
+logging.getLogger('scrapy').propagate = False
+logging.getLogger('scrapy.utils').propagate = False
 
 
 class QuotesSpider(scrapy.Spider):
+    """Small scrapy Spider."""
     name = "Watch Later"
     start_urls = [
         'file:///tmp/wl.html'
     ]
+    custom_settings = {
+    	"LOG_ENABLED": False,
+    	"LOG_LEVEL": 'ERROR',
+    }
 
     def parse(self, response):
-        print("response =", response)  # DEBUG
+        """Parse the response to print a JSON file of all videos in /tmp/wl.html"""
         i = 0
         for item in response.css('td.pl-video-title'):
-            print("i =", i)  # DEBUG
-            print("item =", item)  # DEBUG
-            i += 1
+            i += 1  # enumerate(...) was not working!
             video = item.css('a.pl-video-title-link')[0]
             author = item.css('div.pl-video-owner')[0]
-            yield {
+            res = {
                 'id': i,
                 'href': video.xpath('@href').extract_first().replace('&index=%i&list=WL'%i, ''),
                 'title': video.css('a::text').extract_first().strip(),
                 'author': author.css('a::text').extract_first().strip()
             }
+            print(res)
+            #yield res
 
 
 # End of youtube_playlist_spider_scrapy.py
