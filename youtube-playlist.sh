b82355cefe8dbe85f28df1263f84816431f61332
Lilian Besson <lbesson@ens-cachan.fr>
2018-05-16 14:21:40 +0200

    Fixed bug with youtube-playlist.sh when the title was wrongly read and took more than one line.

--- youtube-playlist.sh
+++ youtube-playlist.sh
@@ -71,7 +71,7 @@ dlplaylist() {
     $READ || exit
 
     # Create the directory
-    title=$(grep "<title>" "${out}" | head | sed s/"<title>"// | sed s/"^[ ]*"// | sed s_"</title>"__ | sed s_"<link.*"_""_ )
+    title=$(grep "<title>" "${out}" | head | sed s/"<title>"// | sed s/"^[ ]*"// | sed s_"</title>"__ | sed s_"<link.*"_""_ | tail -n1)
     echo -e "Apparently, the playlist's title is : '${yellow}${title}${white}'. Are you OK with it ? (${magenta}[Enter]${white} if OK)."
     $READ || exit
 
