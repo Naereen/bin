e2875d721f007bceaeaac916846bdb7036582c7b
Lilian Besson <lilian.besson@crans.org>
2019-07-04 10:35:16 +0200

    Typos

--- admissibilite_envoi_mail_thunderbird.sh
+++ admissibilite_envoi_mail_thunderbird.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-thunderbird -compose "to=lilian.besson@inria.fr;agreg 2017 <info.agreg.2017@listes.ens-rennes.fr>;info agreg profs <info.agreg.profs@listes.ens-rennes.fr>,from=lilian.besson@inria.fr,subject='Résultats d'admission disponibles',message=/home/lilian/Bureau/mail.txt"
+thunderbird -compose "to=lilian.besson@inria.fr;agreg 2018 <info.agreg.2018@listes.ens-rennes.fr>;info agreg profs <info.agreg.profs@listes.ens-rennes.fr>,from=lilian.besson@inria.fr,subject='Résultats d'admission disponibles',message=/home/lilian/Bureau/mail.txt"
