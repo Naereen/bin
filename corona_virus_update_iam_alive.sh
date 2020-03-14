d2fd6600af278dca7f11290038836071f8bc4817
Lilian Besson <lilian.besson@crans.org>
2020-03-14 21:32:06 +0100

    https://perso.crans.org/besson/vivant

---
+++ corona_virus_update_iam_alive.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+notify-send "Building https://perso.crans.org/besson/coronavirus.fr.html ..."
+cd ~/web-sphinx/
+
+rm -vf -- ./coronavirus.*.rst~
+git add -- coronavirus.*.rst
+
+git commit -m "Auto updating https://perso.crans.org/besson/coronavirus.fr.html"
+git push && clear
+
+make && clear
+
+notify-send "Updated https://perso.crans.org/besson/coronavirus.fr.html : YOUPI JE SUIS (ENCORE) VIVANT"
+echo "Updated https://perso.crans.org/besson/coronavirus.fr.html : YOUPI JE SUIS (ENCORE) VIVANT" | figlet | lolcat
