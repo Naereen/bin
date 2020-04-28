fe3033437a3a8da57a72b0af560165bd5a2be053
Lilian Besson <lilian.besson@crans.org>
2020-04-28 17:09:57 +0200

    Added documentation for these tiny scripts, written during the Coronavirus 2020 outbreak.

--- launch-watch-git-pull-Version-Jaune.sh
+++ launch-watch-git-pull-Version-Jaune.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
+#
+# Launch this script to do "git pull" in Version-Jaune, every hour
+# See https://github.com/Pokemon-via-GitHub-contre-Covid-2020-fr/Version-Jaune
+#
+# Author: Lilian BESSON
+# Email: Lilian.BESSON[AT]crans[DOT]org
+# Web version: https://perso.crans.org/besson/bin/launch-watch-git-pull-Version-Jaune.sh
+# Web version (2): https://bitbucket.org/lbesson/bin/src/master/launch-watch-git-pull-Version-Jaune.sh
+# Date: 2020-04-24 17:48
+#
+# Usage:
+# $ launch-watch-git-pull-Version-Jaune.sh 15
+#
+# will do "git pull" every 15 minutes instead.
+#
+
+watch_duration="${1:-60}"
+duration="$((${watch_duration} * 60))"
 
 watch \
-    --interval $((15 * 60)) \
+    --interval "${duration}" \
     "git pull \
         | tee /tmp/Version-Jaune.log \
         && notify-send \
